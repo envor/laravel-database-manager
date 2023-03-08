fd0a352ded6c9eeede0300f8f1db63a17f236038
= <inman.turbo@gmail.com>
2023-03-08 11:47:45 -0500

    add fake database manager

--- src/DatabaseManager.php
+++ src/DatabaseManager.php
@@ -15,6 +15,40 @@ class DatabaseManager
 
         $manager = config('database-manager.managers.'.$driver);
 
-        return new $manager;
+        return $this->registerDatabaseManager($manager)->getDatabaseManager();
+    }
+
+    protected function registerDatabaseManager(string $manager)
+    {
+        // first we need to check if the driver is already bound
+        if (app()->bound(DatabaseManagerContract::class)) {
+            // if it is, we need to check if it's the same as the one we're trying to register
+            // or if it's a fake
+            $instance = app()->make(DatabaseManagerContract::class);
+            if (
+                $instance instanceof FakeDatabaseManager ||
+                $instance instanceof $manager
+            ) {
+                // if it is, we don't need to do anything
+                return $this;
+            }
+
+            // if it's not, we need to unbind it
+            app()->forgetInstance(DatabaseManagerContract::class);
+        }
+
+        app()->bind(DatabaseManagerContract::class, $manager);
+
+        return $this;
+    }
+    
+    public function getDatabaseManager(): DatabaseManagerContract
+    {
+        return app(DatabaseManagerContract::class);
+    }
+
+    public function fake()
+    {
+        return $this->registerDatabaseManager(FakeDatabaseManager::class);
     }
 }
